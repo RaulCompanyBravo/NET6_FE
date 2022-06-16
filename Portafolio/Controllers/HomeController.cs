6994317c9e39dcf3b95f1e0e6e6371acf2667d7f
Raul Company Bravo <bravo.company.raul@gmail.com>
2022-06-16 20:04:41 +0200

    Modelos a vistas

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -27,7 +27,46 @@ namespace Portafolio.Controllers
 
             return View(persona);
             */
-            return View();
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
+        }
+
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() { 
+                new Proyecto
+                    {
+                        Titulo = "Amazon",
+                        Descripcion = "E-Commerce realizado en ASP.NET Core",
+                        Link = "https://amazon.es",
+                        ImagenUrl = "/img/amazon.png"
+                    },
+            
+                new Proyecto
+                    {
+                        Titulo = "New York Times",
+                        Descripcion = "Pa´gina de noticias en React",
+                        Link = "https://nytimes.com",
+                        ImagenUrl = "/img/nyt.png"
+                    },
+                new Proyecto
+                    {
+                        Titulo = "Reddir",
+                        Descripcion = "Red social para compartir en comunidades",
+                        Link = "https://amazon.es",
+                        ImagenUrl = "/img/reddit.png"
+                    },
+                new Proyecto
+                    {
+                        Titulo = "Steam",
+                        Descripcion = "Tienda en línea para comprar videojuegos",
+                        Link = "https://store.steampowered.es",
+                        ImagenUrl = "/img/steam.png"
+                    },
+
+            };
+
         }
 
         public IActionResult Privacy()
